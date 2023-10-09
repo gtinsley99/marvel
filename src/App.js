03536e8071091d70894161b00944dfb99387ec9b
Zaid <zedpx101@outlook.com>
2023-10-09 14:19:09 +0100

    homepage - refactoring

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import SplashScreen from './components/splashScreen.js';
 import './App.css';
-import Homepage from './Pages/Homepage';
+import Home from './Pages/Home.js';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-        setTimeout(() => {
-            setLoading(false)  
-        }, 3000)
-  },[]);
-
+  
   return (
     <div className="App">
-      {loading ? <SplashScreen /> : (
-      <div className='homepage'>
-        <Homepage />
-      </div>
-      )}
+      <Home/>
     </div>
   );
 }
