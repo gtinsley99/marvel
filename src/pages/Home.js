8ad15d906dd579c684c9992c164dee4837d47766
awaghat99 <awaghat99@gmail.com>
2023-10-09 16:17:37 +0100

    homepage and navbar complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,30 @@
-import React from "react";
+import Description from "../components/Description";
+import { useEffect } from "react";
+import SplashScreen from "../components/splashScreen";
+import { Link } from "react-router-dom";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = (props) => {
+  useEffect(() => {
+    setTimeout(() => {
+      props.setLoading(false);
+    }, 3000);
+  }, [props]);
+
+  return (
+    <div className="homepage">
+      {props.loading ? (
+        <SplashScreen />
+      ) : (
+        <div className="homepage">
+          <h1>Title</h1>
+          <Description className="descHome" />
+          <Link to="/characters">
+            <button className="button">Explore the characters</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
