fa9a484bf2a12ada42fdaf57386271b265059221
Zaid <zedpx101@outlook.com>
2023-10-10 12:26:37 +0100

    added register and login  page with functionality on it

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { useState } from "react";
+import { Route } from "react-router-dom";
+import LoginComponent from "../components/Login";
 
 const Login = () => {
-  return <div>Login</div>;
+
+  const [token, setToken] = useState('');
+
+  const handleLogin = (newToken) => {
+    setToken(newToken)
+  };
+
+  return(
+    <div>
+      {token ? (
+        <div>
+          
+        </div>
+      ) : (
+        ""
+      )}
+      <LoginComponent/>
+    </div>
+  )
 };
 
 export default Login;
