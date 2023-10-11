afdcd9e92061da08c4b4061a2a9db3b5afde9974
awaghat99 <awaghat99@gmail.com>
2023-10-11 12:12:56 +0100

    style login and register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import LoginComponent from "../components/Login";
+import RegistrationForm from "../components/Register";
+import PlaceHolder from "../images/marvel-placeholder.jpg";
+import CoverImage from "../images/avengers-cover.png";
 
 const Login = (props) => {
   const [token, setToken] = useState("");
@@ -10,15 +13,25 @@ const Login = (props) => {
   };
 
   return (
-    <div>
-      <LoginComponent
-        cookie={props.cookie}
-        setCookie={props.setCookie}
-        removeCookie={props.removeCookie}
-        user={props.user}
-        setUser={props.setUser}
-        setLoggedIn={props.setLoggedIn}
-      />
+    <div className="whole-login">
+      <div className="img-holder">
+        <img src={CoverImage} alt="placeholder" />
+      </div>
+      <div className="login-register">
+        <LoginComponent
+          cookie={props.cookie}
+          setCookie={props.setCookie}
+          removeCookie={props.removeCookie}
+          user={props.user}
+          setUser={props.setUser}
+          setLoggedIn={props.setLoggedIn}
+        />
+        <RegistrationForm
+          cookie={props.cookie}
+          setCookie={props.setCookie}
+          removeCookie={props.removeCookie}
+        />
+      </div>
     </div>
   );
 };
