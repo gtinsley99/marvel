1fa0412a5b4fe0ed30962e6f7a83c9f3c70fc6d6
awaghat99 <awaghat99@gmail.com>
2023-10-11 10:53:05 +0100

    render profile or login depending on logged in status

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,26 @@
 import React from "react";
 import { useState } from "react";
-import { Route } from "react-router-dom";
 import LoginComponent from "../components/Login";
 
 const Login = (props) => {
-
-  const [token, setToken] = useState('');
+  const [token, setToken] = useState("");
 
   const handleLogin = (newToken) => {
-    setToken(newToken)
+    setToken(newToken);
   };
 
-  return(
+  return (
     <div>
-      <LoginComponent cookie={props.cookie} setCookie={props.setCookie} removeCookie={props.removeCookie} user={props.user} setUser={props.setUser} setLoggedIn={props.setLoggedIn}/>
+      <LoginComponent
+        cookie={props.cookie}
+        setCookie={props.setCookie}
+        removeCookie={props.removeCookie}
+        user={props.user}
+        setUser={props.setUser}
+        setLoggedIn={props.setLoggedIn}
+      />
     </div>
-  )
+  );
 };
 
 export default Login;
