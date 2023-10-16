086e7f48b10253e250c5d0d9d51af987653032b7
awaghat99 <awaghat99@gmail.com>
2023-10-16 12:56:35 +0100

    add pagination to characters

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { useState } from "react";
 import LoginComponent from "../components/Login/Login";
 import RegistrationForm from "../components/Login/Register";
 import CoverImage from "../images/avengers-cover.png";
 
 const Login = (props) => {
-
   return (
     <div className="whole-login">
       <div className="img-holder">
